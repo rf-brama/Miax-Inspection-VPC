7a6a10778888529858a39f8eeb62df9f89d87c29
Balasubramanian Ramachandran <balasubramanianramachandran@rfny133.local>
2022-06-01 01:33:30 +0530

    added open cidr as variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "vpcendpointeast" {
   type        = string
   default     = "vpce-05e5eca677ac5e145"
 }
+
+variable "cidropen" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR"
+  type        = string
+  default     = "0.0.0.0/0"
+}
